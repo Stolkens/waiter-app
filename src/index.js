a89bd787ce5adfbc0c90df9c617801fd03a1012d
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-05-14 17:17:19 +0200

    Init project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+
+const container = document.querySelector('#root')
+const root = createRoot(container)
+
+root.render(
+  <Provider store={store}>
+    <App tab="home"/>
+  </Provider>
+);
